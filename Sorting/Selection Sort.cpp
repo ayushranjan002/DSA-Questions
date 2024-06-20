76231b32a5e0338650a4be3e7766943a40b7a902
ayushranjan002 <143868869+ayushranjan002@users.noreply.github.com>
2024-06-21 00:49:37 +0530

    Create Selection Sort.cpp

---
+++ Sorting/Selection Sort.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h> 
+void selectionSort(vector<int>& arr, int n)
+{   
+    int minindex;
+    for(int i=0;i<n-1;i++){ 
+        int minindex=i;
+     
+        for(int j=i+1;j<n;j++){
+           if (arr[minindex] > arr[j]) 
+             minindex = j;
+      
+        }
+         swap(arr[minindex], arr[i]);
+    }
+}

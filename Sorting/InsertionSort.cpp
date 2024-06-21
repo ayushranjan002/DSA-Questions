f1864cc8259910f889ba8812b5cba6b0ba7626ab
ayushranjan002 <143868869+ayushranjan002@users.noreply.github.com>
2024-06-21 20:01:19 +0530

    Create InsertionSort.cpp

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i=1;i<n;i++){
+        int current=arr[i];
+        int j=i-1;
+        while(arr[j]>current && j>=0){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=current;
+    }
+}

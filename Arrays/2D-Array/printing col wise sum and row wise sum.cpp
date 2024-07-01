b71bd602ce0d8a9598e8d4c11ca4c5faf5a8850f
ayushranjan002 <143868869+ayushranjan002@users.noreply.github.com>
2024-07-01 21:33:21 +0530

    Create printing col wise sum and row wise sum.cpp

---
+++ Arrays/2D-Array/printing col wise sum and row wise sum.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void row_wise_sum(int** arr, int n, int m) {//n for row and m for column
+	int sum=0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+           		sum+=arr[i][j];
+            }
+           cout<<"the sum for "<<i+1<<"th "<< "row is "<<sum<<endl; 
+        }
+    }
+
+
+void col_wise_sum(int** arr, int n, int m) {
+    for (int j = 0; j < m; j++) { // Iterate over columns
+        int sum = 0;
+        for (int i = 0; i < n; i++) { // Iterate over rows
+            sum += arr[i][j];
+        }
+        cout << "The sum for " << j+1 << "th column is " << sum << endl;
+    }
+}
+
+int main() {
+    int n, m;
+    cout << "Enter length of row and column:" << endl;
+    cin >> n >> m;
+
+    // Allocate dynamic 2D array
+    int** arr = new int*[n];
+    for (int i = 0; i < n; i++) {
+        arr[i] = new int[m];
+    }
+
+    cout << "Enter elements:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "The array is:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    row_wise_sum(arr, n, m);
+    col_wise_sum(arr, n, m);
+    return 0;
+}

30e49a0285223ee4cea50a432612657dc212fa28
ayushranjan002 <143868869+ayushranjan002@users.noreply.github.com>
2024-07-23 16:31:22 +0530

    Create reverse string(code 360).cpp

---
+++ Recursion/reverse string(code 360).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h> 
+
+void reverse(string& str, int i, int j ) {
+
+   
+    //base case
+    if(i>j)
+        return ;
+
+    swap(str[i], str[j]);
+    i++;
+    j--;
+
+    //Recursive call
+    reverse(str,i,j);
+
+}
+
+string reverseString(string str)
+{
+	// Write your code here.
+	reverse(str,0,str.length()-1);
+	return str;
+}

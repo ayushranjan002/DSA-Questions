b80d016583fa8532cdbf32ca208a4101b59fd368
ayushranjan002 <143868869+ayushranjan002@users.noreply.github.com>
2024-09-14 10:36:15 +0530

    Create Longest Subarray With Maximum Bitwise AND(LC-2419).cpp

---
+++ Basic Concept/Longest Subarray With Maximum Bitwise AND(LC-2419).cpp
@@ -0,0 +1,54 @@
+// optimized approach
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int maxi=0;
+        int c = 0;
+        int maxlength = 0;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] > maxi) {
+                maxi = nums[i];  
+            }
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == maxi){
+                c++;
+                maxlength = max(maxlength, c);
+            }
+            else{
+                c = 0;
+            }
+        }
+        return maxlength;
+    }
+};
+//brute force
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int ans = 0;        
+        int maxlength = 0;   
+
+        for(int i = 0; i < nums.size(); i++) {
+            int temp = nums[i];  
+            
+            for(int j = i; j < nums.size(); j++) {
+                temp &= nums[j];  
+
+                if(temp > ans) {  
+                    ans = temp;   
+                    maxlength = j - i + 1;  
+                } 
+                else if(temp == ans) {  
+                    maxlength = max(maxlength, j - i + 1);  
+                }
+
+                // Break the loop if temp becomes 0 since further ANDing will not help
+                if(temp == 0) break;
+            }
+        }
+        return maxlength;  
+    }
+};
